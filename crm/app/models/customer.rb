eeeecd88f8b39a5d18ea77fde2e03b9f12c264ac
ChrisSmithArt <chrissmithart@proton.me>
2024-06-17 11:38:34 -0500

    added active record, works properly

--- crm/app/models/customer.rb
+++ crm/app/models/customer.rb
@@ -1,2 +1,5 @@
 class Customer < ApplicationRecord
+  def self.ransackable_attributes(auth_object = nil)
+    ["EmailAddress", "FullName", "Notes", "PhoneNumber", "created_at", "id", "id_value", "updated_at"]
+  end
 end
